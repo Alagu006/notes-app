788ef53972e21dc03007482dbbbcd4eea6d624fa
Alagu006 <plalagappan2006@gmail.com>
2026-03-19 08:31:17 +0530

    this is a final checkup

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://notes-app-one-beige.vercel.app/' // Replace with your Vercel URL
+    'https://notes-app-chi-blue.vercel.app/' // Replace with your Vercel URL
   ],
   credentials: true
 }));
